1ceb4cc31c030ea0bdc3d4211826b86e099f6de5
rademrdja <radem98@gmail.com>
2023-04-27 17:24:07 +0200

    Implement routes related to developers

---
+++ api/routes/developers.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Developer = require("../model/developer");
+
+router.get("/", (request, response, next) => {
+  Developer.find()
+    .select("name specialisation _id")
+    .exec()
+    .then((doc) => {
+      const res = {
+        count: doc.length,
+        developers: doc,
+      };
+      response.status(200).json(res);
+    })
+    .catch((err) => {
+      response.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.post("/", (request, response, next) => {
+  const developer = new Developer({
+    _id: new mongoose.Types.ObjectId(),
+    name: request.body.name,
+    specialisation: request.body.specialisation,
+  });
+  developer
+    .save()
+    .then((res) => {
+      console.log(res);
+      response.status(201).json({
+        message: "Create developer success",
+        createdDeveloper: developer,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      response.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.get("/:developerId", (request, response, next) => {
+  const id = request.params.developerId;
+  Developer.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log(doc);
+      if (doc) {
+        response.status(200).json(doc);
+      } else {
+        response.status(404).json({
+          error: "Not found",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      response.status(500).json({ error: err });
+    });
+});
+
+router.patch("/:developerId", (request, response, next) => {
+  const id = request.params.developerId;
+  const updateObject = {};
+  for (let props in request.body) {
+    updateObject[props] = request.body[props];
+  }
+  Developer.updateOne(
+    { _id: id },
+    {
+      $set: updateObject,
+    }
+  )
+    .exec()
+    .then((docs) => {
+      response.status(200).json(docs);
+    })
+    .catch((err) => {
+      response.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.delete("/:developerId", (request, response, next) => {
+  const id = request.params.developerId;
+  Developer.deleteOne({ _id: id })
+    .exec()
+    .then((res) => {
+      response.status(200).json({
+        message: "Developer deleted",
+      });
+    })
+    .catch((err) => {
+      response.status(500).json({
+        error: err,
+      });
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,4 +1,42 @@
 const express = require("express");
 const app = express();
 
+const developerRoutes = require("./api/routes/developers");
+
+mongoose.connect(process.env.CONNECTION_STRING);
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((request, response, next) => {
+  // preventing cors error
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "*");
+  if (request.method === "OPTIONS") {
+    response.header(
+      "Access-Control-Allow-Methods",
+      "PUT, GET, POST, DELETE, PATCH"
+    );
+    return response.status(200).json();
+  }
+  next();
+});
+
+app.use("/developers", developerRoutes);
+
+app.use((request, response, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, request, response, next) => {
+  return response.status(error.status || 500).json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
 module.exports = app;
