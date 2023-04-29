6804e73a75f2533b00e8a1c2da07f9959540bc28
arodissim <doraisimiljkovic@gmail.com>
2023-04-29 13:25:19 +0200

    Add POST and GET by id routes for projects

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -35,4 +35,47 @@ router.delete("/:projectId", (request, response, next) => {
     });
 });
 
+router.post("/", (request, response, next) => {
+  Developer.findById(request.body.developerId)
+    .then((developer) => {
+      if (!developer) {
+        response.status(404).json({
+          message: "Developer not found.",
+        });
+      }
+      const project = new Project({
+        _id: new mongoose.Types.ObjectId(),
+        name: request.body.name,
+        durationInMonths: request.body.durationInMonths,
+        developerId: developer._id,
+      });
+      return project.save();
+    })
+    .then((res) => {
+      response.status(201).json(res);
+    })
+    .catch((err) => {
+      response.status(500).json(err);
+    });
+});
+
+router.get("/:projectId", (request, response, next) => {
+  const id = request.params.projectId;
+  Project.findById(id)
+    .exec()
+    .then((doc) => {
+      if (doc) {
+        response.status(200).json(doc);
+      } else {
+        response.status(404).json({
+          error: "Project not found",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      response.status(500).json({ error: err });
+    });
+});
+
 module.exports = router;
